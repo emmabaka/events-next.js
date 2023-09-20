0e24d0adcd239f0d064339f159e347e0c3605e82
Emma Baka <112560476+emmabaka@users.noreply.github.com>
2023-09-20 13:40:30 +0300

    feat: add newsletter route

--- src/components/input/NewsletterRegister/NewsletterRegister.tsx
+++ src/components/input/NewsletterRegister/NewsletterRegister.tsx
@@ -1,25 +1,44 @@
-import { FormEvent } from "react";
+import { FormEvent, MutableRefObject, useRef } from "react";
 import s from "./NewsletterRegister.module.scss";
 
 function NewsletterRegister() {
+  const emailInputRef: MutableRefObject<HTMLInputElement | null> = useRef(null);
+  const formRef: MutableRefObject<HTMLFormElement | null> = useRef(null);
+
   function registrationHandler(event: FormEvent) {
     event.preventDefault();
+    console.log(event);
+    const enteredEmail = emailInputRef.current!.value;
 
-    // fetch user input (state or refs)
-    // optional: validate input
-    // send valid data to API
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+    if (emailPattern.test(enteredEmail)) {
+      fetch("/api/newsletter", {
+        method: "POST",
+        body: JSON.stringify({ email: enteredEmail }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => console.log(data));
+      formRef.current!.reset();
+    } else {
+      alert("Invalid email");
+    }
   }
 
   return (
     <section className={s.newsletter}>
       <h2>Sign up to stay updated!</h2>
-      <form onSubmit={registrationHandler}>
+      <form onSubmit={registrationHandler} ref={formRef}>
         <div className={s.control}>
           <input
             type="email"
             id="email"
             placeholder="Your email"
             aria-label="Your email"
+            ref={emailInputRef}
           />
           <button>Register</button>
         </div>
