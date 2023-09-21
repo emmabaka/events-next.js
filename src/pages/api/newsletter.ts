8d9339e3b7645138a4a0157fe9154e004502d00b
Emma Baka <112560476+emmabaka@users.noreply.github.com>
2023-09-21 14:39:32 +0300

    feat: add error handling to newsletter route

--- src/pages/api/newsletter.ts
+++ src/pages/api/newsletter.ts
@@ -1,6 +1,23 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { MongoClient } from "mongodb";
 
+async function connectDatabase() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://emma:Hx8Z5qlIHpQJaDnN@cluster1.evdwtxy.mongodb.net/events?retryWrites=true&w=majority"
+  );
+
+  return client;
+}
+
+async function insertDocument(
+  client: MongoClient,
+  document: { email: string }
+) {
+  const db = client.db();
+
+  await db.collection("newsletter").insertOne(document);
+}
+
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
@@ -10,14 +27,22 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
       return;
     }
 
-    const client = await MongoClient.connect(
-      "mongodb+srv://emma:Hx8Z5qlIHpQJaDnN@cluster1.evdwtxy.mongodb.net/events?retryWrites=true&w=majority"
-    );
-    const db = client.db();
+    let client: MongoClient | undefined;
 
-    await db.collection("newsletter").insertOne({ email: userEmail });
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({ message: "Connecting to the database failed!" });
+      return;
+    }
 
-    client.close();
+    try {
+      await insertDocument(client, { email: userEmail });
+      client!.close();
+    } catch (error) {
+      res.status(500).json({ message: "Inserting data failed!" });
+      return;
+    }
 
     res.status(201).json({ message: "Signed up!" });
   }
